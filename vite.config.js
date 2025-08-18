ac7b7fd3efb6350a015852e5550f92901d6acdff
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2025-08-19 01:01:54 +0400

    fix: js path on build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/MaterialKolor-to-json/' : '/',
   root: './src',
   server: {
     port: 3000,
