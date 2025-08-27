82baab43a393f4d6a1cae8b302d914322dfdba11
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2025-08-28 02:46:40 +0400

    fix: resolve paths issue on production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/MaterialKolor-to-json/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '' : '/',
   root: './src',
   server: {
     port: 3000,
